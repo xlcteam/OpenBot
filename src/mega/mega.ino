fbdec76da2a7869ea6a406277676f6259d8bd672
Jakub Gál <44644846+JakubGal@users.noreply.github.com>
2020-03-06 23:42:36 +0100

    first_steps
    First working program
    + rpi camera procesing
    + movements

--- src/mega/mega.ino
+++ src/mega/mega.ino
@@ -5,6 +5,7 @@ int spd = 150;
 byte a, b;
 int angle;
 int anti_angle;
+int x_b, y_b, x_g, y_g;
 LiquidCrystal lcd(26, 27, 28, 29, 40, 41);
 
 void setup() {
@@ -15,10 +16,17 @@ void setup() {
   butons_setup();
   NXT_setup();
   NXT_nort_set();
-
+  rpi_setup();
 }
 void loop() {
-//TODO piny
+  //TODO piny
+  x_b = get_x_ball();
+  x_g = get_x_goal();
+  x_b *= -1;
+  if (x_b < 0) {
+    x_b = 360 - get_x_ball();
+  }
+
   if (digitalRead(3)) {
     Wire.requestFrom(8, 2);
 
@@ -38,13 +46,52 @@ void loop() {
     Serial.print(a);
     Serial.print('\t');
     Serial.println(b);
-    movement(anti_angle, 510,NXT_angle());
+    movement(anti_angle, 510, NXT_angle());
   }
   else {
-    movement(90, 500,NXT_angle());
+    // movement(90, 500, NXT_angle());
+        Serial.print("Mo");
+        Serial.print('\t');
+        Serial.print(x_b);
+        Serial.print('\t');
+        Serial.println(x_g);
+        
+    if (x_g != 62) {
+      //na_mieste(x_g);
+      if (x_b != 298) {
+        //Serial.println( x_b);
+        movement(x_b, 510, x_g);
+      }
+      else {
+        na_mieste(x_g);
+      }
+    } else {
+      //na_mieste(NXT_angle());
+      if (x_b != 298) {
+        //Serial.println( x_b);
+        movement(x_b, 510, NXT_angle());
+      }
+      else {
+        na_mieste(NXT_angle());
+      }
+    }
+
   }
 
-  //na_mieste(NXT_angle());
+  // na_mieste(x_g);
+  //Serial.println( x_b);
+  /*
+    if (x_g != 70) {
+    if (x_b != 70) {
+      movement(x_b, 420, x_g);
+    }
+    else {
+      off_motors();
+    }
+    }
+    else {
+    off_motors();
+    }*/
 }
 
 void na_mieste(int vstup) {
@@ -62,7 +109,7 @@ void movement(int smer , int rychlost , int feedback) {
   int compensation;
   compensation = PID(speeds, 0, feedback);
 
-  
+
   rotate_motor(1, speeds[0] + compensation);
   rotate_motor(2, speeds[1] + compensation);
   rotate_motor(3, speeds[2] + compensation);
---
+++ src/mega/rpi.ino
@@ -0,0 +1,47 @@
+void rpi_setup() {
+  Serial3.begin(115200);
+  pinMode(30, OUTPUT);
+  pinMode(31, OUTPUT);
+  pinMode(39, OUTPUT);
+  pinMode(38, OUTPUT);
+  digitalWrite(30, LOW);
+  digitalWrite(31, LOW);
+  digitalWrite(39, LOW);
+  digitalWrite(38, LOW);
+}
+int get_x_ball() {
+  int value;
+  digitalWrite(30, HIGH);
+  while (!Serial3.available())
+    value = Serial3.parseInt();
+  while (Serial3.available())Serial3.read();
+  digitalWrite(30, LOW);
+  return value > 0 ? (176 - value) / 2.8 : (176 + value) / 2.8;
+}
+int get_y_ball() {
+  int value;
+  digitalWrite(31, HIGH);
+  while (!Serial3.available())
+    value = Serial3.parseInt();
+  while (Serial3.available())Serial3.read();
+  digitalWrite(31, LOW);
+  return value > 0 ? (176 - value) / 2.8 : (176 + value) / 2.8;
+}
+int get_x_goal() {
+  int value;
+  digitalWrite(39, HIGH);
+  while (!Serial3.available())
+    value = Serial3.parseInt();
+  while (Serial3.available())Serial3.read();
+  digitalWrite(39, LOW);
+  return value > 0 ? (176 - value) / 2.8 : (176 + value) / 2.8;
+}
+int get_y_goal() {
+  int value;
+  digitalWrite(38, HIGH);
+  while (!Serial3.available())
+    value = Serial3.parseInt();
+  while (Serial3.available())Serial3.read();
+  digitalWrite(38, LOW);
+  return value > 0 ? (176 - value) / 2.8 : (176 + value) / 2.8;
+}
